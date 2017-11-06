2d2fa5718861b86c55c00b2c31e0c5a73b42bf9e
Matt Heck <matthewaheck7u@gmail.com>
2017-11-06 10:04:52 -0600

    more practice for 11/6

--- script/example.js
+++ script/example.js
@@ -1,19 +1,47 @@
-var table = 5;    //unit of table
-var operator = 'addition';  //type of calculation(defaults to addition)
-var i = 1;  //set counter to 1
-var msg = '';  //message
 
-if(operator === 'addition'){  //if the operator says addition
-  while (i < 20) {          //while the counter is less than 11
-    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />'; //calculation
-    i++; //add one to the counter
-  }
-} else {  //otherwise
-    while (i < 20) { //while the counter is less than 11
-      msg += i + ' x ' + table + ' = ' + (i * table) + '<br />'; //calculation
-    i++;  //add 1 to the counter
-  }
+// 1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
+
+var customName = document.getElementById('customname');
+var randomize = document.querySelector('.randomize');
+var story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  return array[Math.floor(Math.random()*array.length)];
 }
-//write the message to the page
-var el = document.getElementById('blackboard');
-el.innerHTML = msg;
+
+// 2. RAW TEXT STRINGS
+
+var storyText = 'It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but he was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+
+var insertX = ['Willy the Goblin',
+'Big Daddy',
+'Father Christmas'];
+
+var insertY = ['the soup kitchen',
+'Disneyland',
+'the White House'];
+
+var insertZ = ['spontaneously combusted',
+'melted into a puddle on the sidewalk',
+'turned into a slug and crawled away'];
+
+3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+  if(customName.value != '') {
+    var name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    var weight = Math.round(300);
+    var temperature =  Math.round(94);
+
+  }
+
+  story.textContent = '' ;
+  story.style.visibility = 'visible';
+}
