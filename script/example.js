06ea50750622ee127a1ea741d0e55fbe44ea1cc5
Matt Heck <matthewaheck7u@gmail.com>
2017-11-07 10:01:01 -0600

    js practice for 11/7

--- script/example.js
+++ script/example.js
@@ -6,7 +6,8 @@ var randomize = document.querySelector('.randomize');
 var story = document.querySelector('.story');
 
 function randomValueFromArray(array){
-  return array[Math.floor(Math.random()*array.length)];
+  var random = Math.floor(Math.random()*array.length);
+  return array[random];
 }
 
 // 2. RAW TEXT STRINGS
@@ -25,23 +26,39 @@ var insertZ = ['spontaneously combusted',
 'melted into a puddle on the sidewalk',
 'turned into a slug and crawled away'];
 
-3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
+// 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
 
 randomize.addEventListener('click', result);
 
+
+
 function result() {
+  var newStory = storyText;
+
+  var xItem = randomValueFromArray(insertX);
+  var yItem = randomValueFromArray(insertY);
+  var zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replace(':insertx:', xItem);
+  newStory = newStory.replace(':insertx:', xItem);
+  newStory = newStory.replace(':inserty:', yItem);
+  newStory = newStory.replace(':insertz:', zItem);
+
+
 
   if(customName.value != '') {
     var name = customName.value;
+    newStory = newStory.replace('Bob', name);
 
   }
 
   if(document.getElementById("uk").checked) {
-    var weight = Math.round(300);
-    var temperature =  Math.round(94);
-
+    var weight = Math.round(300 * 0.0714286) + ' stone';
+    var temperature =  Math.round((94-32) * 5 / 9) + ' centigrade';
+    newStory = newStory.replace('94 farenheit', temperature);
+    newStory = newStory.replace('300 pounds', weight);
   }
 
-  story.textContent = '' ;
+  story.textContent = newStory ;
   story.style.visibility = 'visible';
 }
